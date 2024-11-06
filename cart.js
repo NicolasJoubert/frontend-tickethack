b1b35eb5e8c8f13e52739b273734a10914000d4c
NicolasJoubert <nicolasjoubert@icloud.com>
2024-11-06 14:02:26 +0100

    add cart

--- cart.js
+++ cart.js
@@ -1,18 +1,29 @@
-document
-  .querySelector("#remove-ticket-btn")
-  .addEventListener("click", function () {
-    const cartContainer = (document.querySelector(
-      "#cart-container"
-    ).style.display = "none");
+// document
+//   .querySelector("#remove-ticket-btn")
+//   .addEventListener("click", function () {
+//     const cartContainer = (document.querySelector(
+//       "#cart-container"
+//     ).style.display = "none");
 
-    //   const emptyCartMessage = `
-    //   <div id="container">
-    //     <div id="cart">
-    //       <p>No ticket in your cart.</p>
-    //       <p>Why not plan your trip?</p>
-    //     </div>
-    //   </div>
-    // `;
+//     //   const emptyCartMessage = `
+//     //   <div id="container">
+//     //     <div id="cart">
+//     //       <p>No ticket in your cart.</p>
+//     //       <p>Why not plan your trip?</p>
+//     //     </div>
+//     //   </div>
+//     // `;
 
-    //   cartContainer.innerHTML = emptyCartMessage;
+//     //   cartContainer.innerHTML = emptyCartMessage;
+//   });
+
+fetch(`http://localhost:3000/cart/`)
+  .then((response) => response.json())
+  .then((data) => {
+    if (!data.allTripsofCart || data.allTripsofCart.length === 0) {
+      document.querySelector("#container").style.display = "flex";
+    } else {
+      document.querySelector("#cart-container").style.display = "flex";
+      console.log(data);
+    }
   });
